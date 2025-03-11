f52a7c43961198af73560f181b0eaebac696f10b
hlemay <h.lemay@yahoo.com>
2025-03-11 22:22:30 +0000

    ladder implementation

---
+++ src/ladder.cpp
@@ -0,0 +1,93 @@
+#include "ladder.h"
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void error(string word1, string word2, string msg) {
+    cerr << "Error: [ \'" << word1 << "\' , \'" << word2 << "\' ] - " << msg << endl;
+    exit(1);
+}
+
+void load_words(set<string> & word_list, const string& file_name) {
+    ifstream file(file_name);
+    if (!file) { cerr << "Error: unable to open file." << endl; }
+    string word;
+    while (file >> word) {
+        word_list.insert(word);
+    }
+    file.close();
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    int len1 = str1.size(), len2 = str2.size();
+    if (abs(len1 - len2) > d) return false;
+
+    int diff = 0, i = 0, j = 0;
+    while (i < len1 && j < len2) {
+        if (str1[i] != str2[j]) {
+            if (++diff > 1) return false;
+            if (len1 > len2) ++i;
+            else if (len1 < len2) ++j;
+            else { ++i; ++j; }
+        } else { ++i; ++j; }
+    }
+    return true;
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    if (begin_word == end_word) {
+        cout << "Error: Start and end words have to be different." << endl;
+        return {};
+    }
+
+    queue<vector<string>> ladder_queue;
+    set<string> visited;
+
+    ladder_queue.push({begin_word});
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+
+        string last_word = ladder.back();
+
+        for (const string& word : word_list) {
+            if(is_adjacent(last_word, word) && visited.find(word) == visited.end()) {
+                visited.insert(word);
+                vector<string> new_ladder = ladder;
+                new_ladder.push_back(word);
+
+                if(word == end_word) return new_ladder;
+
+                ladder_queue.push(new_ladder);
+            }
+        }
+    }
+    return {};
+}
+                
+void print_word_ladder(const vector<string>& ladder) {
+    if (ladder.empty()) {
+        cout << "No word ladder exists." << endl;
+        return;
+    }
+    for (size_t i = 0; i < ladder.size(); ++i) {
+        cout << ladder[i];
+        if (i < ladder.size() - 1) cout << " -> ";
+    }
+    cout << endl;
+}
+    
+void verify_word_ladder();
+    
