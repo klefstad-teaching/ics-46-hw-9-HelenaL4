831dcce96315ad172779a68113f21bdbe094185f
hlemay <h.lemay@yahoo.com>
2025-03-16 02:49:18 +0000

    fixed ladder print

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -82,9 +82,9 @@ void print_word_ladder(const vector<string>& ladder) {
         cout << "No word ladder exists." << endl;
         return;
     }
+    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); ++i) {
-        cout << ladder[i];
-        if (i < ladder.size() - 1) cout << " -> ";
+        cout << ladder[i] << " ";
     }
     cout << endl;
 }
