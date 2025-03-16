d786c228bb24e238894b94aad041e991f9faadc8
hlemay <h.lemay@yahoo.com>
2025-03-16 02:33:20 +0000

    dijkstras implementation

---
+++ src/dijkstras.cpp
@@ -0,0 +1,68 @@
+#include "dijkstras.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.numVertices;
+    vector<int> distance(n, INF);
+    previous.assign(n, -1);
+    vector<bool> visited(n, false);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, source});
+    distance[source] = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (visited[u]) continue;
+        visited[u] = true;
+
+        for (const Edge& neighbor : G[u]) {
+            int v = neighbor.dst;
+            int weight = neighbor.weight;
+
+            if(!visited[v] && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push({distance[v], v});
+            }
+        }
+    }
+    return distance;
+}
+    
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> path;
+    if (distances[destination] == INF) return path;
+
+    for (int v = destination; v != -1; v = previous[v])
+        path.push_back(v);
+
+    reverse(path.begin(), path.end());
+    return path;
+}
+    
+void print_path(const vector<int>& v, int total) {
+    if (v.empty()) {
+        cout << "No path exists." << endl;
+        return;
+    }
+    cout << "Shortest Path: ";
+    for(size_t i = 0; i < v.size(); ++i) {
+        cout << v[i];
+        if (i < v.size() - 1) cout << "->";
+    }
+    cout << " (Cost: " << total << ")" << endl;
+}
+
+        
+        
